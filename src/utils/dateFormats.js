c0776bebf6df0cb3ef6c43e173f47e0fc17e3d40
Divyanshu Maithani <div.blackcat@gmail.com>
2017-11-24 15:59:41 +0530

    Update dateFormats - add date_time_no_millis

--- src/utils/dateFormats.js
+++ src/utils/dateFormats.js
@@ -3,6 +3,7 @@ const dateFormats = {
 	basic_date: "yyyyMMdd",
 	basic_date_time: "yyyyMMdd'T'HHmmss.fffzzz",
 	basic_date_time_no_millis: "yyyyMMdd'T'HHmmsszzz",
+	date_time_no_millis: "yyyy-MM-dd'T'HH:mm:sszzz",
 	basic_time: "HHmmss.fffzzz",
 	basic_time_no_millis: "HHmmsszzz",
 	epoch_millis: "epoch_millis",
