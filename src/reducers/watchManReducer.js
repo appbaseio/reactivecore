91f96cee7829faa41af4300f55026cba73c006ca
Deepak Grover <deepakgrover333@gmail.com>
2017-12-28 16:54:49 +0530

    Fix linting in watcManReducer

--- src/reducers/watchManReducer.js
+++ src/reducers/watchManReducer.js
@@ -1,5 +1,22 @@
 import { WATCH_COMPONENT, REMOVE_COMPONENT } from '../constants';
 
+function getWatchList(depTree) {
+	const list = Object.values(depTree);
+	const components = [];
+
+	list.forEach((item) => {
+		if (typeof item === 'string') {
+			components.push(item);
+		} else if (Array.isArray(item)) {
+			components.push(...item);
+		} else if (typeof item === 'object' && item !== null) {
+			components.push(...getWatchList(item));
+		}
+	});
+
+	return [...new Set(components)];
+}
+
 export default function watchManReducer(state = {}, action) {
 	if (action.type === WATCH_COMPONENT) {
 		const watchList = getWatchList(action.react);
@@ -19,31 +36,15 @@ export default function watchManReducer(state = {}, action) {
 		return newState;
 	} else if (action.type === REMOVE_COMPONENT) {
 		const newState = { ...state };
-		for (const component in newState) {
+		Object.keys(newState).forEach((component) => {
 			if (component === action.component) {
 				delete newState[component];
 			} else {
-				newState[component] = newState[component].filter(item => item !== action.component);
+				newState[component] = newState[component]
+					.filter(item => item !== action.component);
 			}
-		}
+		});
 		return newState;
 	}
 	return state;
-}
-
-function getWatchList(depTree) {
-	const list = Object.values(depTree);
-	const components = [];
-
-	list.forEach((item) => {
-		if (typeof item === 'string') {
-			components.push(item);
-		} else if (Array.isArray(item)) {
-			components.push(...item);
-		} else if (typeof item === 'object' && item !== null) {
-			components.push(...getWatchList(item));
-		}
-	});
-
-	return [...new Set(components)];
 }
