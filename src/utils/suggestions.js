795b0f78a3e5f98ba4eef196410e5c0bc6846ffc
Deepak Grover <deepakgrover333@gmail.com>
2018-01-03 19:28:33 +0530

    add suggestions helper methods

---
+++ src/utils/suggestions.js
@@ -0,0 +1,59 @@
+// flattens a nested array
+const flatten = arr => (
+	arr.reduce((flat, toFlatten) =>
+		flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), [])
+);
+
+const extractSuggestion = (val) => {
+	switch (typeof val) {
+		case 'string':
+			return val.toLowerCase();
+		case 'object':
+			if (Array.isArray(val)) {
+				return flatten(val);
+			}
+			return null;
+
+		default:
+			return val;
+	}
+};
+
+const getSuggestions = (fields, suggestions, currentValue) => {
+	let suggestionsList = [];
+	let labelsList = [];
+
+	const populateSuggestionsList = (val) => {
+		// check if the suggestion includes the current value
+		// and not already included in other suggestions
+		const isWordMatch = currentValue.trim().split(' ').some(term => val.includes(term));
+		if (isWordMatch && !labelsList.includes(val)) {
+			const option = {
+				label: val,
+				value: val,
+			};
+			labelsList = [...labelsList, val];
+			suggestionsList = [...suggestionsList, option];
+		}
+	};
+
+	suggestions.forEach((item) => {
+		fields.forEach((field) => {
+			const label = item._source[field];
+			if (label) {
+				const val = extractSuggestion(label);
+				if (val) {
+					if (Array.isArray(val)) {
+						val.forEach(suggestion => populateSuggestionsList(suggestion));
+					} else {
+						populateSuggestionsList(val);
+					}
+				}
+			}
+		});
+	});
+
+	return suggestionsList;
+};
+
+export default getSuggestions;
