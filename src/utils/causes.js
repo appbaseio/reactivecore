b78018f86a0b249501ef39e295f542a38c2a1546
Divyanshu Maithani <div.blackcat@gmail.com>
2018-08-10 15:52:25 +0530

    Add causes

---
+++ src/utils/causes.js
@@ -0,0 +1,11 @@
+// A map of causes leading to changes in components
+
+const ENTER_PRESS = 'ENTER_PRESS';
+const SUGGESTION_SELECT = 'SUGGESTION_SELECT';
+
+const causes = {
+	ENTER_PRESS,
+	SUGGESTION_SELECT,
+};
+
+export default causes;
