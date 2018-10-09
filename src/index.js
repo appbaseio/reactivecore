62b600e0b717ab2e88c3d2e209f9a0fb47960bea
Deepak Grover <deepakgrover333@gmail.com>
2018-10-09 16:18:51 +0530

    Add support for redux devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { STORE_KEY } from './constants';
 
 export const storeKey = STORE_KEY;
 
+const composeEnhancers
+	= typeof window === 'object'
+	&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
 export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
 		initialState,
-		applyMiddleware(thunk),
+		enhancer,
 	);
 }
