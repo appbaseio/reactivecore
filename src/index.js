c1154977f016f7617e2af478d4147fe8c2437656
kuldeep <kuldepsaxena155@gmail.com>
2018-10-18 03:58:06 +0530

    feat(): Added vue types

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import VueTypes from './utils/vueTypes';
 import rootReducer from './reducers';
 import { STORE_KEY } from './constants';
+import * as Actions from './actions';
+import * as helper from './utils/helper';
 
+export { Actions };
+export { helper };
 export const storeKey = STORE_KEY;
-
+export const vueTypes = VueTypes;
 export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
