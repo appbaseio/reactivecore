236ccfb35bce5accebc70ee56ca8b0161871045f
kuldeep <kuldepsaxena155@gmail.com>
2018-10-19 04:03:53 +0530

    feat(): Export suggestions & causes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import rootReducer from './reducers';
 import { STORE_KEY } from './constants';
 import * as Actions from './actions';
 import * as helper from './utils/helper';
+import * as suggestions from './utils/suggestions';
+import * as causes from './utils/causes';
 
-export { Actions };
-export { helper };
-export const storeKey = STORE_KEY;
-export const vueTypes = VueTypes;
+
+const storeKey = STORE_KEY;
+const vueTypes = VueTypes;
+export { helper, causes, suggestions, Actions, vueTypes, storeKey };
 export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
