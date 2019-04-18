6abc2520bdaa77fae2deaa502b80872cf2a4ac90
kuldeep <kuldepsaxena155@gmail.com>
2019-04-18 08:27:25 +0530

    chore: export reducers

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ import * as helper from './utils/helper';
 import Suggestions from './utils/suggestions';
 import polyfills from './utils/polyfills';
 import Causes from './utils/causes';
+import valueReducer from './reducers/valueReducer';
+import queryReducer from './reducers/queryReducer';
+import queryOptionsReducer from './reducers/queryOptionsReducer';
+import dependencyTreeReducer from './reducers/dependencyTreeReducer';
+
 
 const storeKey = STORE_KEY;
 const suggestions = Suggestions;
 const causes = Causes;
-export { helper, causes, suggestions, Actions, storeKey, polyfills };
+const Reducers = {
+	valueReducer, queryOptionsReducer, queryReducer, dependencyTreeReducer,
+};
+export { helper, causes, suggestions, Actions, storeKey, polyfills, Reducers };
 
 const composeEnhancers
 	= typeof window === 'object'
