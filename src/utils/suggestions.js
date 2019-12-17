e008fbcfa6e749c27b8cc54c0c4580823e8868b6
bietkul <kuldepsaxena155@gmail.com>
2019-12-17 10:18:06 +0530

    fix: autosuggest with ascii chars

--- src/utils/suggestions.js
+++ src/utils/suggestions.js
@@ -20,6 +20,28 @@ const extractSuggestion = (val) => {
 	}
 };
 
+function replaceDiacritics(s) {
+	let str = s;
+
+	const diacritics = [
+		/[\300-\306]/g, /[\340-\346]/g, // A, a
+		/[\310-\313]/g, /[\350-\353]/g, // E, e
+		/[\314-\317]/g, /[\354-\357]/g, // I, i
+		/[\322-\330]/g, /[\362-\370]/g, // O, o
+		/[\331-\334]/g, /[\371-\374]/g, // U, u
+		/[\321]/g, /[\361]/g, // N, n
+		/[\307]/g, /[\347]/g, // C, c
+	];
+
+	const chars = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+
+	for (let i = 0; i < diacritics.length; i += 1) {
+		str = str.replace(diacritics[i], chars[i]);
+	}
+
+	return str;
+}
+
 const getSuggestions = (fields, suggestions, currentValue, suggestionProperties = []) => {
 	let suggestionsList = [];
 	let labelsList = [];
@@ -31,9 +53,9 @@ const getSuggestions = (fields, suggestions, currentValue, suggestionProperties 
 			.trim()
 			.split(' ')
 			.some(term =>
-				String(val)
+				String(replaceDiacritics(val))
 					.toLowerCase()
-					.includes(term));
+					.includes(replaceDiacritics(term)));
 		if (isWordMatch && !labelsList.includes(val)) {
 			const defaultOption = {
 				label: val,
