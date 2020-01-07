31363fe9a3bf03ff015574314cb474e8edbabcd6
jyash97 <jyash97@gmail.com>
2020-01-07 19:22:51 +0530

    feat: add record analytics action

--- src/actions/analytics.js
+++ src/actions/analytics.js
@@ -35,3 +35,57 @@ export function updateAnalyticsConfig(analyticsConfig) {
 		analyticsConfig,
 	};
 }
+
+export function recordResultClick(searchPosition) {
+	return (dispatch, getState) => {
+		const {
+			config,
+			analytics: { searchId },
+			headers,
+		} = getState();
+		const { url, app, credentials } = config;
+		if (config.analytics && searchId) {
+			const parsedHeaders = headers;
+			delete parsedHeaders['X-Search-Query'];
+			fetch(`${url}/${app}/_analytics`, {
+				method: 'POST',
+				headers: {
+					...parsedHeaders,
+					'Content-Type': 'application/json',
+					Authorization: `Basic ${btoa(credentials)}`,
+					'X-Search-Id': searchId,
+					'X-Search-Click': true,
+					'X-Search-ClickPosition': searchPosition + 1,
+				},
+			});
+		}
+	};
+}
+
+export function recordSuggestionClick(searchPosition) {
+	return (dispatch, getState) => {
+		const {
+			config,
+			analytics: { suggestionsSearchId },
+			headers,
+		} = getState();
+		const { url, app, credentials } = config;
+		if (config.analytics && suggestionsSearchId) {
+			const parsedHeaders = headers;
+			delete parsedHeaders['X-Search-Query'];
+			fetch(`${url}/${app}/_analytics`, {
+				method: 'POST',
+				headers: {
+					...parsedHeaders,
+					'Content-Type': 'application/json',
+					Authorization: `Basic ${btoa(credentials)}`,
+					'X-Search-Id': suggestionsSearchId,
+					'X-Search-Suggestions-Click': true,
+					...(searchPosition !== undefined && {
+						'X-Search-Suggestions-ClickPosition': searchPosition + 1,
+					}),
+				},
+			});
+		}
+	};
+}
