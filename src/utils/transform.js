40fad75630c53fd992d4706e7817596d0c7dc148
bietkul <kuldepsaxena155@gmail.com>
2020-06-11 11:35:14 +0530

    fix: minor fix

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -112,7 +112,7 @@ export const getRSQuery = (componentId, props, execute = true) => {
 			enableSynonyms: props.enableSynonyms,
 			selectAllLabel: props.selectAllLabel,
 			pagination: props.pagination,
-			queryString: props.queryString || undefined,
+			queryString: props.queryString,
 		};
 	}
 	return null;
