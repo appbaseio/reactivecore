b73cedea85c76333689770e04ffe05da8b8af9e1
bietkul <kuldepsaxena155@gmail.com>
2020-08-27 12:06:05 +0530

    fix: click analytics for appbase clusters

--- src/actions/analytics.js
+++ src/actions/analytics.js
@@ -36,58 +36,117 @@ export function updateAnalyticsConfig(analyticsConfig) {
 	};
 }
 
-export function recordResultClick(searchPosition) {
+function recordClick({
+	url,
+	app,
+	credentials,
+	headers,
+	documentId,
+	clickPosition,
+	queryId,
+	clickType,
+}) {
+	if (!documentId) {
+		console.warn('ReactiveSearch: document id is required to record the click analytics');
+	} else {
+		// Use record click API for clusters
+		fetch(`${url}/${app}/_analytics/click`, {
+			method: 'PUT',
+			body: JSON.stringify({
+				query_id: queryId,
+				click_type: clickType || 'result',
+				click_on: {
+					[documentId]: clickPosition + 1,
+				},
+			}),
+			headers: {
+				...headers,
+				'Content-Type': 'application/json',
+				Authorization: `Basic ${btoa(credentials)}`,
+			},
+		});
+	}
+}
+
+export function recordResultClick(searchPosition, documentId) {
 	return (dispatch, getState) => {
 		const {
 			config,
 			analytics: { searchId },
 			headers,
+			appbaseRef: { url, protocol, credentials },
 		} = getState();
-		const { url, app, credentials } = config;
+		const { app } = config;
+		const esURL = `${protocol}://${url}`;
 		if (config.analytics && searchId) {
 			const parsedHeaders = headers;
 			delete parsedHeaders['X-Search-Query'];
-			const parsedURL = (url || '').replace(/\/+$/, '');
-			fetch(`${parsedURL}/${app}/_analytics`, {
-				method: 'POST',
-				headers: {
-					...parsedHeaders,
-					'Content-Type': 'application/json',
-					Authorization: `Basic ${btoa(credentials)}`,
-					'X-Search-Id': searchId,
-					'X-Search-Click': true,
-					'X-Search-ClickPosition': searchPosition + 1,
-				},
-			});
+			const parsedURL = (esURL || '').replace(/\/+$/, '');
+			if (parsedURL.includes('scalr.api.appbase.io')) {
+				fetch(`${parsedURL}/${app}/_analytics`, {
+					method: 'POST',
+					headers: {
+						...parsedHeaders,
+						'Content-Type': 'application/json',
+						Authorization: `Basic ${btoa(credentials)}`,
+						'X-Search-Id': searchId,
+						'X-Search-Click': true,
+						'X-Search-ClickPosition': searchPosition + 1,
+					},
+				});
+			} else {
+				recordClick({
+					url: parsedURL,
+					app,
+					credentials,
+					parsedHeaders,
+					documentId,
+					clickPosition: searchPosition,
+					queryId: searchId,
+				});
+			}
 		}
 	};
 }
 
-export function recordSuggestionClick(searchPosition) {
+export function recordSuggestionClick(searchPosition, documentId) {
 	return (dispatch, getState) => {
 		const {
 			config,
 			analytics: { suggestionsSearchId },
 			headers,
+			appbaseRef: { url, protocol, credentials },
 		} = getState();
-		const { url, app, credentials } = config;
-		if (config.analytics && suggestionsSearchId) {
+		const { app } = config;
+		const esURL = `${protocol}://${url}`;
+		if (config.analytics && suggestionsSearchId && searchPosition !== undefined) {
 			const parsedHeaders = headers;
 			delete parsedHeaders['X-Search-Query'];
-			const parsedURL = (url || '').replace(/\/+$/, '');
-			fetch(`${parsedURL}/${app}/_analytics`, {
-				method: 'POST',
-				headers: {
-					...parsedHeaders,
-					'Content-Type': 'application/json',
-					Authorization: `Basic ${btoa(credentials)}`,
-					'X-Search-Id': suggestionsSearchId,
-					'X-Search-Suggestions-Click': true,
-					...(searchPosition !== undefined && {
+			const parsedURL = (esURL || '').replace(/\/+$/, '');
+			if (parsedURL.includes('scalr.api.appbase.io')) {
+				fetch(`${parsedURL}/${app}/_analytics`, {
+					method: 'POST',
+					headers: {
+						...parsedHeaders,
+						'Content-Type': 'application/json',
+						Authorization: `Basic ${btoa(credentials)}`,
+						'X-Search-Id': suggestionsSearchId,
+						'X-Search-Suggestions-Click': true,
 						'X-Search-Suggestions-ClickPosition': searchPosition + 1,
-					}),
-				},
-			});
+					},
+				});
+			} else {
+				recordClick({
+					url: parsedURL,
+					app,
+					credentials,
+					parsedHeaders,
+					documentId,
+					clickPosition: searchPosition,
+					clickType: 'suggestion',
+					queryId: suggestionsSearchId,
+				});
+			}
 		}
 	};
 }
