a4d76037c67953a8fe23cb23ca22d1cc8fab997c
bietkul <kuldepsaxena155@gmail.com>
2022-01-06 17:26:49 +0530

    fix: buckets map error

--- src/reducers/compositeAggsReducer.js
+++ src/reducers/compositeAggsReducer.js
@@ -7,7 +7,10 @@ export default function compositeAggsReducer(state = {}, action) {
 			= Object.values(action.aggregations) && Object.values(action.aggregations)[0];
 		const fieldName = Object.keys(action.aggregations)[0];
 		if (!aggsResponse) return state;
-		const buckets = aggsResponse.buckets || [];
+		let buckets = [];
+		if (aggsResponse.buckets && Array.isArray(aggsResponse.buckets)) {
+			buckets = aggsResponse.buckets;
+		}
 		const parsedAggs = buckets.map((bucket) => {
 			// eslint-disable-next-line camelcase
 			const { doc_count, key, [fieldName]: hitsData } = bucket;
