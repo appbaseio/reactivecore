968e4a6a4b81f2384e57c7a459c8e8dce92e8218
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-05-04 12:46:12 +0530

    feat: add new suggestions-config props to valid props list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -115,6 +115,8 @@ export const validProps = [
 	'enablePredictiveSuggestions',
 	'applyStopwords',
 	'customStopwords',
+	'enableIndexSuggestions',
+	'enableFeaturedSuggestions',
 ];
 
 export const CLEAR_ALL = {
