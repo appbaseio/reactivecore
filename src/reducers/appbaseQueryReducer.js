b18a76adc2e98ec31724476335cb1bf8146f5a0d
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-08-01 07:09:58 +0530

    fix: store lastUsedAppbaseQuery as array

--- src/reducers/appbaseQueryReducer.js
+++ src/reducers/appbaseQueryReducer.js
@@ -2,7 +2,7 @@ import { SET_APPBASE_QUERY } from '../constants';
 
 export default function appbaseQueryReducer(state = {}, action) {
 	if (action.type === SET_APPBASE_QUERY) {
-		return { ...state, ...(action.query || []) };
+		return [...(action.query || [])];
 	}
 	return state;
 }
