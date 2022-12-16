20106a1cb3e0c1caedecb83c395af535fd8e3bea
bietkul <kuldepsaxena155@gmail.com>
2022-12-16 13:09:14 +0530

    fix: add support to track component's registered timestamp

--- src/actions/component.js
+++ src/actions/component.js
@@ -1,11 +1,30 @@
-import { ADD_COMPONENT, REMOVE_COMPONENT, WATCH_COMPONENT } from '../constants';
+import {
+	ADD_COMPONENT,
+	REMOVE_COMPONENT,
+	WATCH_COMPONENT,
+	SET_REGISTERED_COMPONENT_TIMESTAMP,
+} from '../constants';
 
 import { executeQuery } from './query';
 
-export function addComponent(component) {
+function addComponentToList(component) {
 	return {
 		type: ADD_COMPONENT,
 		component,
+	};
+}
+
+function addComponentTimestamp(component, timestamp) {
+	return {
+		type: SET_REGISTERED_COMPONENT_TIMESTAMP,
+		component,
+		timestamp,
+	};
+}
+export function addComponent(component) {
+	return (dispatch) => {
+		dispatch(addComponentToList(component));
+		dispatch(addComponentTimestamp(component, new Date().getTime()));
 	};
 }
 
