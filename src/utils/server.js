8ac8eaf2e742c92eb5b957d5a833a323f8bc1bf4
bietkul <kuldepsaxena155@gmail.com>
2023-02-02 13:20:42 +0530

    fix: getServerResults export

--- src/utils/server.js
+++ src/utils/server.js
@@ -127,90 +127,67 @@ const getServerResults = () => {
 				} else {
 					promiseFunc = output;
 				}
-				return promiseFunc.then(() => {
-					if (storeReference) {
-						const extractedState = storeReference.getState();
-						const {
-							components,
-							config,
-							appbaseRef,
-							queryOptions,
-							internalValues,
-							props,
-							queryList,
-							dependencyTree,
-						} = extractedState;
+				return promiseFunc
+					.then(() => {
+						if (storeReference) {
+							const extractedState = storeReference.getState();
+							const {
+								components,
+								config,
+								appbaseRef,
+								queryOptions,
+								internalValues,
+								props,
+								queryList,
+								dependencyTree,
+							} = extractedState;
 
-						let { queryLog } = extractedState;
+							let { queryLog } = extractedState;
 
-						let finalQuery = [];
-						let appbaseQuery = {}; // Use object to prevent duplicate query added by react prop
-						let orderOfQueries = [];
-						let hits = {};
-						let aggregations = {};
-						let state = { ...extractedState };
+							let finalQuery = [];
+							let appbaseQuery = {}; // Use object to prevent duplicate query added by react prop
+							let orderOfQueries = [];
+							let hits = {};
+							let aggregations = {};
+							let state = { ...extractedState };
 
-						// Generate finalQuery for search
-						components
-							.filter(t => !t.endsWith('__internal'))
-							.forEach((componentId) => {
-								// eslint-disable-next-line
-								let { queryObj, options } = buildQuery(
-									componentId,
-									dependencyTree,
-									queryList,
-									queryOptions,
-								);
-								if (!queryObj && !options) {
-									return;
-								}
-
-								const query = getRSQuery(
-									componentId,
-									extractPropsFromState(
-										state,
+							// Generate finalQuery for search
+							components
+								.filter(t => !t.endsWith('__internal'))
+								.forEach((componentId) => {
+									// eslint-disable-next-line
+									let { queryObj, options } = buildQuery(
 										componentId,
-										queryOptions && queryOptions[componentId]
-											? { from: queryOptions[componentId].from }
-											: null,
-									),
-								);
+										dependencyTree,
+										queryList,
+										queryOptions,
+									);
+									if (!queryObj && !options) {
+										return;
+									}
 
-								// check if query or options are valid - non-empty
-								if (query && !!Object.keys(query).length) {
-									const currentQuery = query;
-									const dependentQueries = getDependentQueries(
-										state,
+									const query = getRSQuery(
 										componentId,
-										orderOfQueries,
+										extractPropsFromState(
+											state,
+											componentId,
+											queryOptions && queryOptions[componentId]
+												? { from: queryOptions[componentId].from }
+												: null,
+										),
 									);
 
-									let queryToLog = {
-										...{ [componentId]: currentQuery },
-										...Object.keys(dependentQueries).reduce(
-											(acc, q) => ({
-												...acc,
-												[q]: {
-													...dependentQueries[q],
-													execute: false,
-													...(dependentQueries[q].type
-													=== queryTypes.suggestion
-														? { type: 'search' }
-														: {}),
-												},
-											}),
-											{},
-										),
-									};
-									if (
-										[queryTypes.range, queryTypes.term].includes(componentToTypeMap[props[componentId].componentType])
-									) {
-										// Avoid logging `value` for term type of components
-										// eslint-disable-next-line
-										const { value, ...rest } = currentQuery;
+									// check if query or options are valid - non-empty
+									if (query && !!Object.keys(query).length) {
+										const currentQuery = query;
+										const dependentQueries = getDependentQueries(
+											state,
+											componentId,
+											orderOfQueries,
+										);
 
-										queryToLog = {
-											...{ [componentId]: rest },
+										let queryToLog = {
+											...{ [componentId]: currentQuery },
 											...Object.keys(dependentQueries).reduce(
 												(acc, q) => ({
 													...acc,
@@ -226,133 +203,168 @@ const getServerResults = () => {
 												{},
 											),
 										};
-									}
-									// if (!compareQueries(queryToLog, currentQuery, false)) {
-									orderOfQueries = [...orderOfQueries, componentId];
-									queryLog = {
-										...queryLog,
-										[componentId]: queryToLog,
-									};
-									// }
+										if (
+											[queryTypes.range, queryTypes.term].includes(componentToTypeMap[
+												props[componentId].componentType
+											])
+										) {
+											// Avoid logging `value` for term type of components
+											// eslint-disable-next-line
+											const { value, ...rest } = currentQuery;
 
-									if (query) {
-										// Apply dependent queries
-										const dependentQueriesToAppend = getDependentQueries(
-											state,
-											componentId,
-											orderOfQueries,
-										);
-										appbaseQuery = {
-											...appbaseQuery,
-											...{ [componentId]: query },
+											queryToLog = {
+												...{ [componentId]: rest },
+												...Object.keys(dependentQueries).reduce(
+													(acc, q) => ({
+														...acc,
+														[q]: {
+															...dependentQueries[q],
+															execute: false,
+															...(dependentQueries[q].type
+															=== queryTypes.suggestion
+																? { type: 'search' }
+																: {}),
+														},
+													}),
+													{},
+												),
+											};
+										}
+										// if (!compareQueries(queryToLog, currentQuery, false)) {
+										orderOfQueries = [...orderOfQueries, componentId];
+										queryLog = {
+											...queryLog,
+											[componentId]: queryToLog,
 										};
-										Object.keys(dependentQueriesToAppend).forEach((cId) => {
-											if (appbaseQuery[cId]) {
-												appbaseQuery[cId + Math.random()]
-													= dependentQueriesToAppend[cId];
-											} else {
-												appbaseQuery[cId] = dependentQueriesToAppend[cId];
-											}
-										});
+										// }
+
+										if (query) {
+											// Apply dependent queries
+											const dependentQueriesToAppend = getDependentQueries(
+												state,
+												componentId,
+												orderOfQueries,
+											);
+											appbaseQuery = {
+												...appbaseQuery,
+												...{ [componentId]: query },
+											};
+											Object.keys(dependentQueriesToAppend).forEach((cId) => {
+												if (appbaseQuery[cId]) {
+													appbaseQuery[cId + Math.random()]
+														= dependentQueriesToAppend[cId];
+												} else {
+													appbaseQuery[cId]
+														= dependentQueriesToAppend[cId];
+												}
+											});
+										}
 									}
-								}
-							});
+								});
 
-						const handleRSResponse = (res) => {
-							const promotedResults = {};
-							const rawData = {};
-							const customData = {};
-							const allPromises = orderOfQueries.map(component =>
-								new Promise((responseResolve, responseReject) => {
-									handleTransformResponse(res[component], config, component)
-										.then((response) => {
-											if (response) {
-												if (response.promoted) {
-													promotedResults[component]
-															= response.promoted.map(promoted => ({
-															...promoted.doc,
-															_position: promoted.position,
-														}));
-												}
-												rawData[component] = response;
-												// Update custom data
-												if (response.customData) {
-													customData[component] = response.customData;
-												}
+							const handleRSResponse = (res) => {
+								const promotedResults = {};
+								const rawData = {};
+								const customData = {};
+								const allPromises = orderOfQueries.map(component =>
+									new Promise((responseResolve, responseReject) => {
+										handleTransformResponse(
+											res[component],
+											config,
+											component,
+										)
+											.then((response) => {
+												if (response) {
+													if (response.promoted) {
+														promotedResults[component]
+																= response.promoted.map(promoted => ({
+																...promoted.doc,
+																_position:
+																			promoted.position,
+															}));
+													}
+													rawData[component] = response;
+													// Update custom data
+													if (response.customData) {
+														customData[component]
+																= response.customData;
+													}
 
-												if (response.aggregations) {
-													aggregations = {
-														...aggregations,
-														[component]: response.aggregations,
+													if (response.aggregations) {
+														aggregations = {
+															...aggregations,
+															[component]: response.aggregations,
+														};
+													}
+													const hitsObj = response.hits || {};
+													hits = {
+														...hits,
+														[component]: {
+															hits: hitsObj.hits,
+															total:
+																	typeof hitsObj.total
+																	=== 'object'
+																		? hitsObj.total.value
+																		: hitsObj.total,
+															time: response.took,
+														},
 													};
+													responseResolve();
 												}
-												const hitsObj = response.hits || {};
-												hits = {
-													...hits,
-													[component]: {
-														hits: hitsObj.hits,
-														total:
-																typeof hitsObj.total === 'object'
-																	? hitsObj.total.value
-																	: hitsObj.total,
-														time: response.took,
-													},
-												};
-												responseResolve();
-											}
-										})
-										.catch((err) => {
-											responseReject(err);
-										});
-								}));
+											})
+											.catch((err) => {
+												responseReject(err);
+											});
+									}));
 
-							return Promise.all(allPromises).then(() => {
-								state = {
-									queryList,
-									queryOptions,
-									selectedValues: newSelectedValues,
-									internalValues,
-									queryLog,
-									hits,
-									aggregations,
-									promotedResults,
-									customData,
-									rawData,
-									dependencyTree,
-								};
-								return Promise.resolve(JSON.parse(JSON.stringify(state)));
-							});
-						};
+								return Promise.all(allPromises).then(() => {
+									state = {
+										queryList,
+										queryOptions,
+										selectedValues: newSelectedValues,
+										internalValues,
+										queryLog,
+										hits,
+										aggregations,
+										promotedResults,
+										customData,
+										rawData,
+										dependencyTree,
+									};
+									return Promise.resolve(JSON.parse(JSON.stringify(state)));
+								});
+							};
 
-						if (Object.keys(appbaseQuery).length) {
-							finalQuery = Object.values(appbaseQuery);
-							// Call RS API
-							const rsAPISettings = {};
-							if (config.analyticsConfig) {
-								rsAPISettings.recordAnalytics = isPropertyDefined(config.analyticsConfig.recordAnalytics)
-									? config.analyticsConfig.recordAnalytics
-									: undefined;
-								rsAPISettings.userId = isPropertyDefined(config.analyticsConfig.userId)
-									? config.analyticsConfig.userId
-									: undefined;
-								rsAPISettings.enableQueryRules = isPropertyDefined(config.analyticsConfig.enableQueryRules)
-									? config.analyticsConfig.enableQueryRules
-									: undefined;
-								rsAPISettings.customEvents = isPropertyDefined(config.analyticsConfig.customEvents)
-									? config.analyticsConfig.customEvents
-									: undefined;
+							if (Object.keys(appbaseQuery).length) {
+								finalQuery = Object.values(appbaseQuery);
+								// Call RS API
+								const rsAPISettings = {};
+								if (config.analyticsConfig) {
+									rsAPISettings.recordAnalytics = isPropertyDefined(config.analyticsConfig.recordAnalytics)
+										? config.analyticsConfig.recordAnalytics
+										: undefined;
+									rsAPISettings.userId = isPropertyDefined(config.analyticsConfig.userId)
+										? config.analyticsConfig.userId
+										: undefined;
+									rsAPISettings.enableQueryRules = isPropertyDefined(config.analyticsConfig.enableQueryRules)
+										? config.analyticsConfig.enableQueryRules
+										: undefined;
+									rsAPISettings.customEvents = isPropertyDefined(config.analyticsConfig.customEvents)
+										? config.analyticsConfig.customEvents
+										: undefined;
+								}
+
+								return appbaseRef
+									.reactiveSearchv3(finalQuery, rsAPISettings)
+									.then(res => handleRSResponse(res))
+									.catch(err => Promise.reject(err));
 							}
-
-							return appbaseRef
-								.reactiveSearchv3(finalQuery, rsAPISettings)
-								.then(res => handleRSResponse(res))
-								.catch(err => Promise.reject(err));
+							throw new Error('Could not compute server-side initial state of the app!');
+						} else {
+							return null;
 						}
-						throw new Error('Could not compute server-side initial state of the app!');
-					} else {
-						return null;
-					}
-				});
+					})
+					.catch(Promise.reject);
 			}
 			return null;
 		} catch (error) {
@@ -360,4 +372,5 @@ const getServerResults = () => {
 		}
 	};
 };
-export default getServerResults;
+// eslint-disable-next-line
+export { getServerResults };
