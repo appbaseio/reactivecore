0dfaf16d53c0b760ade54e3b481f9eeef9d1bd6b
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-03-15 09:13:38 +0530

    add null check for null range values

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -287,7 +287,9 @@ export const extractPropsFromState = (store, component, customOptions) => {
 							: null;
 				} else {
 					rangeValue
-						= rangeValue && store.aggregations[`${component}__range__internal`].min
+						= rangeValue
+						&& store.aggregations[`${component}__range__internal`].min
+						&& store.aggregations[`${component}__range__internal`].min.value
 							? {
 								start: store.aggregations[`${component}__range__internal`].min.value,
 								end: store.aggregations[`${component}__range__internal`].max.value,
