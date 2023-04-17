ee23191ba9004941b51be92a233bee9ce2eb67ed
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-04-17 16:10:12 +0530

    add action to record ai session usefulness

--- src/actions/analytics.js
+++ src/actions/analytics.js
@@ -85,7 +85,7 @@ export function getRecentSearches(queryOptions = {
 				addParam('min_chars', String(queryOptions.minChars));
 			}
 			if (queryOptions.customEvents) {
-				Object.keys(queryOptions.customEvents).forEach((key: string) => {
+				Object.keys(queryOptions.customEvents).forEach((key) => {
 					addParam(key, queryOptions.customEvents[key]);
 				});
 			}
@@ -96,7 +96,10 @@ export function getRecentSearches(queryOptions = {
 				data: [],
 			});
 		}
-		return fetch(`${parsedURL}/_analytics/${app}/recent-searches?${queryString}`, requestOptions)
+		return fetch(
+			`${parsedURL}/_analytics/${app}/recent-searches?${queryString}`,
+			requestOptions,
+		)
 			.then((res) => {
 				if (res.status >= 500 || res.status >= 400) {
 					return dispatch({
@@ -126,10 +129,7 @@ export function getRecentSearches(queryOptions = {
 }
 
 function recordClick({
-	documentId,
-	clickPosition,
-	analyticsInstance,
-	isSuggestionClick,
+	documentId, clickPosition, analyticsInstance, isSuggestionClick,
 }) {
 	if (!documentId) {
 		console.warn('ReactiveSearch: document id is required to record the click analytics');
@@ -240,11 +240,38 @@ export function recordImpressions(queryId, impressions = []) {
 		} = getState();
 		const esURL = `${protocol}://${url}`;
 		const parsedURL = esURL.replace(/\/+$/, '');
-		if (config.analytics && !parsedURL.includes('scalr.api.appbase.io') && queryId && impressions.length) {
+		if (
+			config.analytics
+			&& !parsedURL.includes('scalr.api.appbase.io')
+			&& queryId
+			&& impressions.length
+		) {
 			analyticsInstance.search({
 				queryID: analyticsInstance.getQueryID(),
 				impressions,
 			});
 		}
+	};
+}
+
+export function recordAISessionUsefulness(sessionId, otherInfo) {
+	return (dispatch, getState) => {
+		const { analyticsRef: analyticsInstance, config } = getState();
+		const userID = config && config.analyticsConfig && config.analyticsConfig.userId;
+		if (!sessionId) {
+			console.warn('ReactiveSearch: AI sessionID is required to record the usefulness of session.');
+		}
+		// Save session usefulness
+		analyticsInstance.saveSessionUsefulness(
+			sessionId,
+			{
+				...otherInfo,
+				userID,
+			},
+			(err, res) => {
+				// eslint-disable-next-line no-console
+				console.log('res', res);
+			},
+		);
 	};
 }
